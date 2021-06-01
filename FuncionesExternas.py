db189f5def7a57815c55b2fee4426c96e0ad338d
santiago574 <60194059+santiago574@users.noreply.github.com>
2021-06-01 17:49:22 -0500

    Add files via upload

---
+++ FuncionesExternas.py
@@ -0,0 +1,11 @@
+#Declaracion de fuciones
+#Funcion sin parametros
+def funcion1 ():
+#COntenido de la funcion
+    print('Has llamado a la funcion 1')
+
+	
+def funcion2 (a,b):
+    #Contenido de la funcion
+    c=int(a)+int(b)
+    return c
---
+++ LlamadoDeFunciones.py
@@ -0,0 +1,8 @@
+#Llamamos las funciones declaradas en otro archivo
+from FuncionesExternas import *
+#Invocamos las funciones
+funcion1()
+#Invocamos la otra funcion
+Q=input('Introdusca un dato:')
+W=input('Introdusca otro dato:')
+print(funcion2(Q,W))
